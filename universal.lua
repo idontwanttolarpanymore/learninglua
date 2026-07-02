9759ebeb66b26872f683457c76a51f41148b3abf
EndAllLarps <idontwanttolarpanymore@gmail.com>
2026-07-02 21:30:56 +0100

    Update universal.lua
    added esp

--- universal.lua
+++ universal.lua
@@ -1,5 +1,5 @@
 local repo = 'https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/'
-
+local esp = loadstring(game:HttpGet("https://raw.githubusercontent.com/whoisthisxor/assets/main/Drawing.lua"))()
 local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
 local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
 local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
@@ -11,8 +11,7 @@ local hum = char:WaitForChild("Humanoid")
 local hrp = char:WaitForChild("HumanoidRootPart")
 
 local Window = Library:CreateWindow({
-
-	Title = 'simple menu',
+	Title = 'universal menu',
     Center = true,
     AutoShow = true,
     TabPadding = 8,
@@ -21,13 +20,11 @@ local Window = Library:CreateWindow({
 
 local tabz = {
     Main = Window:AddTab('Main'),
-    ['UI Settings'] = Window:AddTab('UI Settings'),
+	ESPtab = Window:AddTab('ESPtab'),
+    uisettings = Window:AddTab('UI Settings'),
 }
 
-
 local LeftGroupBox = tabz.Main:AddLeftGroupbox('Character')
-
-
 local wsEnabled = false
 local wsValue = 16
 local jpEnabled = false
@@ -79,7 +76,6 @@ LeftGroupBox:AddSlider('jp slider', {
     end
 })
 
-
 task.spawn(function()
     while true do
         task.wait(0.1)
@@ -92,3 +88,134 @@ task.spawn(function()
     end
 end)
 
+local espbox = tabz.ESPtab:AddLeftGroupbox('Esp main')
+
+local espstuff = {
+	box = nil,
+	name = nil,
+	tracer = nil
+}
+
+local Esp_Enabled = false
+local espColor = Color3.fromRGB(255, 0, 0)
+local showBox = true
+local showName = true
+local showTracer = true
+
+function createESP()
+	if Esp_Enabled == false then return end
+
+	espstuff.box = esp:MakeBox("all", {
+		Visible = true,
+		Color = espColor
+	})
+
+	espstuff.name = esp:MakeName("all", {
+		Visible = true,
+		Color = espColor,
+		Text  = "Name"
+	})
+
+	espstuff.tracer = esp:MakeLine("all", {
+        Visible = true,
+        Color = espColor
+    })
+end
+
+function RemoveESP()
+	if espstuff.box then
+		espstuff.box:Remove() 
+	end
+	if espstuff.name then
+		espstuff.name:Remove() 
+	end
+	if espstuff.tracer then 
+		espstuff.tracer:Remove() 
+	end
+	
+	espstuff = {
+		box = nil,
+		name = nil,
+		tracer = nil
+	}
+end
+
+function UpdateESPColor(color)
+    espColor = color
+    if espstuff.box then 
+		espstuff.box:UpdateColor(color) 
+	end
+    if espstuff.name then
+		espstuff.name:UpdateColor(color) 
+	end
+    if espstuff.tracer then 
+		espstuff.tracer:UpdateColor(color) 
+	end
+end
+
+espbox:AddToggle("esp master toggle", {
+	Text = "enable esp",
+	Default = false,
+	Callback = function(Value)
+		Esp_Enabled = Value
+		if Value == true then
+			createESP()
+		else
+			RemoveESP()
+		end
+	end
+})
+
+espbox:AddLabel("ESP Color"):AddColorPicker("ESP_Color", {
+    Default = Color3.fromRGB(255, 0, 0),
+    Transparency = 0,
+    Callback = function(Value)
+        UpdateESPColor(Value)
+    end
+})
+
+espbox:AddToggle("ESP_Box", {
+    Text = "Show box esp",
+    Default = true,
+    Callback = function(Value)
+        showBox = Value
+        if espstuff.box then
+            espstuff.box:UpdateVisible(Value)
+        end
+    end
+})
+
+espbox:AddToggle("ESP_Name", {
+    Text = "Show name esp",
+    Default = true,
+    Callback = function(Value)
+        showName = Value
+        if espstuff.name then
+            espstuff.name:UpdateVisible(Value)
+        end
+    end
+})
+
+espbox:AddToggle("ESP_Tracer", {
+    Text = "Show tracers",
+    Default = true,
+    Callback = function(Value)
+        showTracer = Value
+        if espstuff.tracer then
+            espstuff.tracer:UpdateVisible(Value)
+        end
+    end
+})
+
+espbox:AddSlider("ESP_TextSize", {
+    Text = "Name Text Size",
+    Default = 20,
+    Min = 10,
+    Max = 50,
+    Rounding = 1,
+    Callback = function(Value)
+        if espstuff.name then
+            espstuff.name:UpdateSize(Value)
+        end
+    end
+})
